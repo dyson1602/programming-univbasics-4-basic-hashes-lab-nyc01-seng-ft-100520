f7cdedea54abfbdc39c5d75f773ec65f5ac3847d
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-13 17:39:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
-  # return an empty hash
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  person = {name: "Chris", occupation: "student"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  grace = {name: "Grace Hopper"}
 end
 
 
